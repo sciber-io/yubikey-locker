8c1da7bb275b8e73c14020156ab13609a65629be
Lukas Ädel <lukas.adel@sciber.io>
2023-11-03 10:18:10 +0100

    otra vez

--- build_it_yourself/build.bat
+++ build_it_yourself/build.bat
@@ -1,3 +1,7 @@
 :: pyinstaller --clean -F --paths=".\venv\lib\site-packages" -n "sciber-yklocker" ..\src\sciber-yklocker.py
-
+@echo off
+echo "Windows build initiated"
 pyinstaller --clean -F -n "sciber-yklocker" ..\src\sciber-yklocker.py
+echo "Pyinstaller finished"
+dir ..\src
+dir ..\src\dist
