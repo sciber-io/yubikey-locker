af692604d834787914e7f249d5b708efaf1ff759
Lukas Ädel <lukas.adel@sciber.io>
2023-11-03 16:21:20 +0100

    reformat code to actually work

--- src/sciber-yklocker.py
+++ src/sciber-yklocker.py
@@ -29,7 +29,6 @@ class ykLock:
         environment = win32profile.CreateEnvironmentBlock(console_user_token, False)
         handle, thread_id ,pid, tid = win32process.CreateProcessAsUser(console_user_token, None, "rundll32.exe user32.dll,LockWorkStation", None, None, True, priority, environment, None, startup)
 
-
         
     def os_detect(self):
         if platform.system() == 'Darwin':
@@ -44,15 +43,15 @@ class ykLock:
     def getOS(self):
         return self.osversion
 
-# Only define Windows code if program is running on Windows
-if ykLock.getOS == 1:
+
+def windowsCode(yklocker):
     #Windows service dependancies
     import win32serviceutil
     import win32service
     import win32event
     import servicemanager
     import socket
-
+    
     #Windows service definition
     class AppServerSvc (win32serviceutil.ServiceFramework):
         _svc_name_ = "SciberYkLocker"
@@ -91,52 +90,51 @@ if ykLock.getOS == 1:
                         servicemanager.LogInfoMsg(f"YubiKey Connected with serial: {info.serial}")
                     if len(list_all_devices()) == 0:
                         servicemanager.LogInfoMsg(f"YubiKey Disconnected. Locking workstation")
-                        ykLock.lockWindows()
+                        yklocker.lockWindows()
 
                 if win32event.WaitForSingleObject(self.hWaitStop, 5000) == win32event.WAIT_OBJECT_0: 
                     break
 
-# Only define Linux code if program is running on Linux
-elif ykLock.getOS == 2:
-    class LinuxCode():
-        def main():
-            from ykman.device import list_all_devices, scan_devices
-            state = None
-            while True:
-                sleep(10)
-                pids, new_state = scan_devices()
-                if new_state != state:
-                    state = new_state  # State has changed
-                for device, info in list_all_devices():
-                    print("YubiKey Connected with serial: " + info.serial)
-                if len(list_all_devices()) == 0:
-                    print("YubiKey Disconnected. Locking workstation")
-                    ykLock.lockLinux()
+    #Start as a service in Windows
+    servicemanager.Initialize()
+    servicemanager.PrepareToHostSingle(AppServerSvc)
+    servicemanager.StartServiceCtrlDispatcher()
+
+
+def linuxCode(yklocker):
+    from ykman.device import list_all_devices, scan_devices
+    state = None
+    while True:
+        sleep(10)
+        pids, new_state = scan_devices()
+        if new_state != state:
+            state = new_state  # State has changed
+        for device, info in list_all_devices():
+            print("YubiKey Connected with serial: " + info.serial)
+        if len(list_all_devices()) == 0:
+            print("YubiKey Disconnected. Locking workstation")
+            yklocker.lockLinux()
 
 
 
 def main(argv):
     import getopt
+    # Create ykLock object
+    yklocker = ykLock()
+    yklocker.os_detect()
 
     # Handle wrong arguments a little bit more smoothly with a try statement
-    try:
-        opts, args = getopt.getopt(argv,"o:",["ostype="])
-        for opt, arg in opts:
-            if opt == '-o':
-                if arg == "win":
-                    #Start as a service in Windows
-                    servicemanager.Initialize()
-                    servicemanager.PrepareToHostSingle(AppServerSvc)
-                    servicemanager.StartServiceCtrlDispatcher()
-                elif arg == "lx":
-                    LinuxCode.main()
-                else: 
-                    print("Please specify win|mac|lx")
-            else:
-                print("Please specify -o and the os <win,mac,lx> to start. Example: yklocker.exe -o win")
-    except:
-        print("Please specify -o and the os <win,mac,lx> to start. Example: yklocker.exe -o win")
-
+    opts, args = getopt.getopt(argv,"o:",["ostype="])
+    for opt, arg in opts:
+        if opt == '-o':
+            if arg == "win":
+                windowsCode(yklocker)
+            elif arg == "lx":
+                linuxCode(yklocker)
+            else: 
+                print("Please specify win|mac|lx")
+        else:
+            print("Please specify -o and the os <win,mac,lx> to start. Example: yklocker.exe -o win")
 
 if __name__ == '__main__':
     import sys
