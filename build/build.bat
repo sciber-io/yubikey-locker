8c218ee37f47a73d0896784ea39384fd189f5f55
Lukas Ädel <lukas.adel@sciber.io>
2023-11-10 11:02:42 +0100

    minor refactoring

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,3 @@
-:: pyinstaller --clean -F --paths=".\venv\lib\site-packages" -n "sciber-yklocker" ..\src\sciber-yklocker.py
 @echo off
 echo "Windows build initiated"
 pyinstaller --clean -F -n "sciber-yklocker" "..\src\sciber-yklocker.py"
--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,2 @@
-# pyinstaller -F --paths=".\venv\lib\site-packages" -n "sciber-yklocker" ..\src\sciber-yklocker.py
-
 echo "Linux build initiated"
 pyinstaller -F -n "sciber-yklocker" ../src/sciber-yklocker.py
