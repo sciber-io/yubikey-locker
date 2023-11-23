5376782ad726dd769527332d8957c709ef922ad3
Lukas Ädel <lukas.adel@sciber.io>
2023-11-23 16:43:45 +0100

    get the renamed module

--- src/emptyModule.py
+++ src/emptyModule.py
@@ -1,4 +1,4 @@
-# Mock to help with running code in non-windows environments
+# Mock to help with running code in non-Windows environments
 class ServiceFramework:
     def __init__(self, args):
         return True
