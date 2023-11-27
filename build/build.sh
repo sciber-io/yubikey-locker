d16903e077351b5c5b5a91d3d7b789380821558a
Lukas Ädel <lukas.adel@sciber.io>
2023-11-27 13:22:08 +0100

    macos plist

--- build/build.sh
+++ build/build.sh
@@ -1,44 +1,17 @@
+if [ "$(uname)" == "Darwin" ]; then
 echo "Linux build initiated"
 pyinstaller -F -n "sciber-yklocker" ../src/sciber-yklocker.py
 
+fi
 # MacOS specifics:
 if [ "$(uname)" == "Darwin" ]; then
+echo "MacOS build initiated"
+pyinstaller -F -n "sciber-yklocker" ../src/sciber-yklocker.py
+# --windowed
+# Get the macos-version of the application to move itself into the desired location, put plist in place, kill itself and start the plist?
+
   # https://pyinstaller.org/en/v4.1/usage.html#building-mac-os-x-app-bundles
 
   # https://medium.com/@fahimhossain_16989/adding-startup-scripts-to-launch-daemon-on-mac-os-x-sierra-10-12-6-7e0318c74de1
-  # /Library/LaunchAgents/com.startup.plist
-  # /Library/LaunchAgents: Per-user agents installed by the admin
-  plist='''
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>EnvironmentVariables</key>
-    <dict>
-      <key>PATH</key>
-      <string>/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:</string>
-    </dict>
-    <key>Label</key>
-    <string>com.startup</string>
-    <key>Program</key>
-    <string>/Users/admin/Scripts/startup/startup.sh</string>
-    <key>RunAtLoad</key>
-    <true/>
-    <key>KeepAlive</key>
-    <false/>
-    <key>LaunchOnlyOnce</key>
-    <true/>
-    <key>StandardOutPath</key>
-    <string>/tmp/startup.stdout</string>
-    <key>StandardErrorPath</key>
-    <string>/tmp/startup.stderr</string>
-    <key>UserName</key>
-    <string>admin</string>
-    <key>GroupName</key>
-    <string>admin</string>
-    <key>InitGroups</key>
-    <true/>
-  </dict>
-</plist>
-  '''
+
 fi
