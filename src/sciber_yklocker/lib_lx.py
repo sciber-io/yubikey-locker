8ba2df47017b15a37cec6afdc2f1287794a3fb24
Lukas Ädel <lukas.adel@sciber.io>
2024-03-12 12:57:28 +0100

    test update macos log show

--- src/sciber_yklocker/lib_lx.py
+++ src/sciber_yklocker/lib_lx.py
@@ -14,6 +14,7 @@ def lock_system(removal_option) -> None:
     if removal_option == RemovalOption.LOCK:
         command = "dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock"
     if removal_option == RemovalOption.LOGOUT:
+        # pkill -SIGKILL -u $(whoami)
         command = "dbus-send --session --type=method_call --print-reply --dest=org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.Logout uint32:1"
 
     os.popen(command)
